1be17addecedf3e285485e16adba9424d85026f5
Fearghal <Fearghal.mcmorrow@gmail.com>
2023-04-23 20:57:43 +0000

    cities api added

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': '5ed3c01d00msh8bab4a391dcfe60p14024cjsna1a16899ecd3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
