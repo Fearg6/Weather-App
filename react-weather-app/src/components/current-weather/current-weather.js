5eb05088503680fc475d05ed9ebe02d0c2ebb66c
Fearghal <Fearghal.mcmorrow@gmail.com>
2023-05-03 18:38:11 +0000

    Fixed some errors

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -1,6 +1,7 @@
 import "./current-weather.css"
 
 const CurrentWeather = ({ data }) => {
+    console.log(data);
     return (
         <div className="weather">
             <div className="top">
@@ -8,7 +9,7 @@ const CurrentWeather = ({ data }) => {
                     <p className="city">{data.city}</p>
                     <p className="weather-description">{data.weather[0].description}</p>
                 </div>
-                <img alt="weather" className="weather-icon" src={`icons/${data.weather.icon}.png`} />
+                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
             </div>
             <div className="bottom">
                 <p className="temperature">{Math.round(data.main.temp)}°C</p>
